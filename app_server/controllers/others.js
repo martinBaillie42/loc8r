f71dd31340fa421a94a18cca837b81aa6b1586d3
martinbaillie42 <martin@martinbaillie.net>
2016-07-08 15:15:52 +0000

    About controller and generic text template updated.

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,4 +1,7 @@
 /* GET 'about' page */
 module.exports.about = function(req, res, next) {
-    res.render('generic-text', { title: 'About'});
+    res.render('generic-text', {
+        title: 'About Loc8r',
+        content: 'Loc8r was created to help people find places to sit down and get a bit of work done.\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Munc sed lorem ac nisi dignissim accumsan.'
+    });
 };
